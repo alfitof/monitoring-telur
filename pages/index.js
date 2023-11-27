ab19c060794945ba610535ee6e54722ef25cd0a1
alfitof <alfito.fbriansyah@gmail.com>
2023-11-27 18:22:24 +0700

    revisi style2

--- pages/index.js
+++ pages/index.js
@@ -142,7 +142,6 @@ const Home = () => {
             Komoditi
           </label>
           <select
-            disabled
             id="countries_disabled"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
           >
